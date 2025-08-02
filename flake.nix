39133dbee45aed95776d9192e1ed3c5a17c3f34a
Silverdev2482 <fidget1206@gmail.com>
2025-08-02 10:16:39 -0500

    Gonna revert to see options for kea database

--- flake.nix
+++ flake.nix
@@ -6,14 +6,25 @@
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
     nixos-router.url = "github:chayleaf/nixos-router";
     my-nvf.url = "github:silverdev2482/nvf";
+    immichUpdatePatch = {
+      url = "https://github.com/NixOS/nixpkgs/pull/430306.patch";
+      flake = false;
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
-    let system = "x86_64-linux";
+    let
+      system = "x86_64-linux";
+      nixpkgs-patched = (import nixpkgs { inherit system; }).applyPatches {
+        name = "nixpkgs-patched";
+        src = nixpkgs;
+        patches = [ inputs.immichUpdatePatch ];
+      };
+      patchedNixOS = import (nixpkgs-patched + /nixos/lib/eval-config.nix);
     in {
       formatter.${system} = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
       nixosConfigurations = {
-        Router-Server = nixpkgs.lib.nixosSystem {
+        Router-Server = patchedNixOS {
           inherit system;
           specialArgs = {
             inherit inputs;
